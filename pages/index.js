8474e69b7ffc3b1e171337242c1baefdc2ec0442
ordervschaos <anzal.ansari@gmail.com>
2022-09-07 19:44:33 -0400

    tailwind

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,23 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div className='pt-6'>
+        <div className="max-w-sm rounded-lg overflow-hidden blue-text shadow-lg bg-black-100 my-3 p-4 mx-auto ">
+          <h1>Hello World</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text ever
+            since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only
+            five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s with
+            the release of Letraset sheets containing Lorem Ipsum passages, and
+            more recently with desktop publishing software like Aldus PageMaker
+            including versions of Lorem Ipsum.
+          </p>
+        </div>
+      </div>
+
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
