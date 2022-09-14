f2728fba1653b0a0d3c7f7a2dc39cb6e8e855334
ordervschaos <anzal.ansari@gmail.com>
2022-09-14 14:25:56 -0400

    move to a module

--- components/CreateRave.tsx
+++ components/CreateRave.tsx
@@ -2,13 +2,14 @@ import { useSession } from "@clerk/nextjs";
 import { useState } from 'react'
 import { createClient } from "@supabase/supabase-js";
 
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
-import { Combobox } from '@headlessui/react'
+
+import SuggestiveDropdown from "./SuggestiveDropdown";
 
 const rave_types = [
-  'Video',
-  'Movie',
-  'Book'
+  { id: 1, name: 'Video' },
+  { id: 2, name: 'Movie' },
+  { id: 3, name: 'Book' },
+  // More users...
 ]
 const supabaseClient = async (supabaseAccessToken) => {
   const supabase = createClient(
@@ -27,25 +28,15 @@ const supabaseClient = async (supabaseAccessToken) => {
 
 
 
-export default function CreateRave() {
+export default function AddRaveForm() {
   const { session } = useSession();
 
   const [newRaveItem, setNewRaveItem] = useState("test");
   const [newRaveReview, setNewRaveReview] = useState("test");
   const [newRaveLink, setNewRaveLink] = useState("test");
-
-  const [query, setQuery] = useState('')
   const [selectedType, setSelectedType] = useState('Book')
 
-  const filteredTypes =
-    query === ''
-      ? rave_types
-      : rave_types.filter((type) => {
-        return type.toLowerCase().includes(query.toLowerCase())
-      })
-  function classNames(...classes) {
-    return classes.filter(Boolean).join(' ')
-  }
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (newRaveItem === "" || newRaveReview === "") {
@@ -76,53 +67,7 @@ export default function CreateRave() {
                   <label htmlFor="company-website" className="block text-sm font-medium text-gray-700">
                     I found something to rave about
                   </label>
-                  <Combobox as="div" value={selectedType} onChange={setSelectedType}>
-                    <Combobox.Label className="block text-sm font-medium text-gray-700">It's a </Combobox.Label>
-                    <div className="relative mt-1">
-                      <Combobox.Input
-                        className="w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm"
-                        onChange={(event) => setQuery(event.target.value)}
-                        displayValue={(type) => type?.name}
-                      />
-                      <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
-                        <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
-                      </Combobox.Button>
-
-                      {filteredTypes.length > 0 && (
-                        <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                          {filteredTypes.map((type) => (
-                            <Combobox.Option
-                              key={type}
-                              value={type}
-                              className={({ active }) =>
-                                classNames(
-                                  'relative cursor-default select-none py-2 pl-3 pr-9',
-                                  active ? 'bg-indigo-600 text-white' : 'text-gray-900'
-                                )
-                              }
-                            >
-                              {({ active, selected }) => (
-                                <>
-                                  <span className={classNames('block truncate', selected && 'font-semibold')}>{type}</span>
-
-                                  {selected && (
-                                    <span
-                                      className={classNames(
-                                        'absolute inset-y-0 right-0 flex items-center pr-4',
-                                        active ? 'text-white' : 'text-indigo-600'
-                                      )}
-                                    >
-                                      <CheckIcon className="h-5 w-5" aria-hidden="true" />
-                                    </span>
-                                  )}
-                                </>
-                              )}
-                            </Combobox.Option>
-                          ))}
-                        </Combobox.Options>
-                      )}
-                    </div>
-                  </Combobox>
+                  <SuggestiveDropdown rave_types={rave_types} selectedType={selectedType} setSelectedType={setSelectedType} />
                 </div>
               </div>
               <div className="space-y-6 bg-white px-4 py-5 sm:p-6">
