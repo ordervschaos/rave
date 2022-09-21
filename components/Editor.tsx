545ed062a8d081ba2528f704fa95ef23e9decf4b
ordervschaos <anzal.ansari@gmail.com>
2022-09-21 13:10:46 -0400

    handle save

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -3,17 +3,29 @@ import { useEffect, useId, useState } from 'react';
 import { EDITOR_JS_TOOLS } from './Editor/tools'
 
 import { createReactEditorJS } from 'react-editor-js'
+import React from 'react';
 const ReactEditorJS = createReactEditorJS()
 
 
 
-export default function Editor({data,setData}) {
+export default function Editor({ data, setData }) {
   const defaultValue = JSON.parse(data)
-  
-  
+
+  const editorCore = React.useRef(null)
+
+  const handleInitialize = React.useCallback((instance) => {
+    editorCore.current = instance
+  }, [])
+
+  const handleSave = React.useCallback(async () => {
+    const savedData = await editorCore.current.save();
+    console.log(savedData)
+  }, [])
+
+
   return (
     <>
-      <ReactEditorJS defaultValue={defaultValue} autofocus={true} tools={EDITOR_JS_TOOLS} placeholder="Tell us the awesome things about the awesome thing" />
+      <ReactEditorJS defaultValue={defaultValue} onInitialize={handleInitialize} autofocus={true} onChange={handleSave} tools={EDITOR_JS_TOOLS} placeholder="Tell us the awesome things about the awesome thing" />
     </>
   )
 }
